c24ca3563aa831dda57eab669d2a8d4bac0c8ae8
Your Name <you@example.com>
2024-07-31 12:31:07 -0300

    cabei

---
+++ Academia.java
@@ -0,0 +1,39 @@
+public class Academia {
+    private String nome;
+    private String tel;
+    private String end;
+
+    public Academia (String nome, String tel, String end){
+        this.nome = nome;
+        this.tel = tel;
+        this.end = end;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+    
+    public void setEnd(String end) {
+        this.end = end;
+    } 
+
+
+
+}
+ 
--- Main.java
+++ Main.java
@@ -1,10 +1,30 @@
 public class Main {
     
     public static void main(String[] args) {
+     
+        Academia rato1 = new Academia("nabos", "34227832", "rua augusto lima, 390");
+        Academia rato2 = new Academia("ratazanas do mal", "92764087", "rua lima de souza, 152");
 
-       
-        System.out.println("Faça sua atividade");
+        System.out.println("obj1:");
+        System.out.println("nome: " + rato1.getNome() + "\ntelefone: " + rato1.getTel() + "\nendereço: " + rato1.getEnd());
+        System.out.println("\nobj2:");
+        System.out.println("nome: " + rato2.getNome() + "\ntelefone: " + rato2.getTel() + "\nendereço: " + rato2.getEnd());
  
+        rato1.setNome("ratozinho");
+        rato1.setTel("21139543");
+        rato1.setEnd("rua do caçador, 456");
+
+        rato2.setNome("nabos");
+        rato2.setTel("98765432");
+        rato2.setEnd("rua dos alves, 120");
+
+        System.out.println("\nobj1:");
+        System.out.println("nome: " + rato1.getNome() + "\ntelefone: " + rato1.getTel() + "\nendereço: " + rato1.getEnd());
+        System.out.println("\nobj2:");
+        System.out.println("nome: " + rato2.getNome() + "\ntelefone: " + rato2.getTel() + "\nendereço: " + rato2.getEnd());
+ 
+
+
     }
  
 }
