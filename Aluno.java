dc9ef63e03f32c162033d20eb56fda1512cb4af0
Your Name <you@example.com>
2024-08-14 12:21:04 -0300

    aa

--- Aluno.java
+++ Aluno.java
@@ -1,87 +1,28 @@
-import java.text.SimpleDateFormat;
+//import java.text.SimpleDateFormat;
+import java.text.ParseException;
 import java.util.Date;
 
-public class Aluno {
-    private String nome;
+public class Aluno extends Pessoa{
+
     private String graduacao;
-    private Date nascimento;
-    private String genero;
-    private double altura;
-    private double peso;
 
-  
-   public Aluno (String nome, String graduacao, Date data, String genero, double altura, double peso){
-       this.nome = nome;
+   public Aluno (String nome, String nascimento, String genero, double altura, double peso, String graduacao) throws ParseException{
+       super(nome, nascimento, genero, altura, peso);
        this.graduacao = graduacao;
-       this.nascimento = data;
-       this.genero = genero;
-       this.altura = altura;
-       this.peso = peso;
     }
 
-public Aluno (String nome, String graduacao, Date nascimento){
-       this(nome, graduacao, nascimento, " ", 0,  0);
+    public Aluno (String nome, String nascimento) throws ParseException{
+       this(nome, nascimento, " ", 0.0,  0.0, nascimento);
     }
 
-
-    public String getNome() {
-        return nome;
-    }
 
     public String graduacao() {
         return graduacao;
     }
-
-    public Date getNascimento() {
-        return nascimento;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public double getAltura() {
-        return altura;
-    }
-
-    public double getPeso() {
-        return peso;
-    }
-
-    public void setNome(String nome){
-        this.nome = nome;
-    }
-
+    
     public void setGraduacao(String graduacao){
         this.graduacao = graduacao;
     }
-
-    public void setNascimento(Date nascimento){
-        this.nascimento = nascimento;
-    }
-
-    public void setGenero(String genero){
-        this.genero = genero;
-    }
-
-    public void setAltura(double altura){
-        this.altura = altura;
-    }
-
-    public void setPeso(double peso){
-        this.peso = peso;
-    }
-
-    @Override
-    public String toString() {
-
-         SimpleDateFormat formato = new SimpleDateFormat("dd/mm/yyyy");    
-
-        return "Aluno [nome=" + nome + ", graduacao=" + graduacao + ", nascimento=" + nascimento + ", genero=" + genero
-                + ", altura=" + altura + ", peso=" + peso + "]";
-    }
-
- 
 
 
 }
--- Main.java
+++ Main.java
@@ -1,62 +1,64 @@
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-    
+
     public static void main(String[] args) throws ParseException {
-     
+
         Academia rato1 = new Academia("nabos", "34227832", "rua augusto lima, 390");
-        SimpleDateFormat formato = new SimpleDateFormat("dd/mm/yyyy");
-        Date data = formato.parse("19/09/2007");
-        Aluno aluno1 = new Aluno ("Hanae", "basico", data, " ", 0.0, 0.0);
-        Date data2 = formato.parse("25/07/2000");
-        Aluno aluno2 = new Aluno ("Natalia", "superior", data2, " ", 0.0, 0.0);
-        Date data3 = formato.parse("07/09/2008");
-        Aluno aluno3 = new Aluno ("nathan", "avançado", data3, "masculino", 57.8, 1.76);
+        String data = "19/09/2007";
+        Aluno aluno1 = new Aluno("Hanae", data, " ", 0.0, 0.0, "basica");
+        String data2 = "25/07/2000";
+        Aluno aluno2 = new Aluno("Natalia", data2, " ", 0.0, 0.0, "intermediario");
+        String data3 = "07/09/2008";
+        Aluno aluno3 = new Aluno("nathan", data3, "masculino", 157.8, 68.5, "avancado");
         rato1.Alunos.add(aluno1);
         rato1.Alunos.add(aluno2);
         rato1.Alunos.add(aluno3);
 
         Academia rato2 = new Academia("ratazanas do mal", "92764087", "rua lima de souza, 152");
-        Date data4 = formato.parse("09/12/2005");
-        Aluno aluno4 = new Aluno ("Maria", "superior", data4, "feminino", 60.5, 1.65);
-        Date data5 = formato.parse("18/05/2003");
-        Aluno aluno5 = new Aluno ("Beatriz", "basico", data5, " ", 0.0, 0.0);
-        Date data6 = formato.parse("23/01/2002");
-        Aluno aluno6 = new Aluno ("João", "avançado", data6, "masculino", 65.2, 1.80);
+        String data4 = "09/12/2005";
+        Aluno aluno4 = new Aluno("Maria", data4, "feminino", 163.5, 65.8, "basico");
+        String data5 = "18/05/2003";
+        Aluno aluno5 = new Aluno("Beatriz", data5, " ", 0.0, 0.0, "basico");
+        String data6 = "23/01/2002";
+        Aluno aluno6 = new Aluno("João", data6, "masculino", 1.80, 89.0, "basico");
         rato2.Alunos.add(aluno4);
         rato2.Alunos.add(aluno5);
         rato2.Alunos.add(aluno6);
+          
 
         System.out.println("obj1:");
         System.out.println("nome: " + rato1.getNome() + "\ntelefone: " + rato1.getTel() + "\nendereço: " + rato1.getEnd());
         System.out.println("\nobj2:");
         System.out.println("nome: " + rato2.getNome() + "\ntelefone: " + rato2.getTel() + "\nendereço: " + rato2.getEnd());
-      
- 
+
+
         rato1.setNome("ratozinho");
         rato1.setTel("21139543");
         rato1.setEnd("rua do caçador, 456");
-         
 
         rato2.setNome("nabos");
         rato2.setTel("98765432");
         rato2.setEnd("rua dos alves, 120");
 
+
         System.out.println("\nobj1:");
         System.out.println("nome: " + rato1.getNome() + "\ntelefone: " + rato1.getTel() + "\nendereço: " + rato1.getEnd());
         System.out.println("\nobj2:");
-        for (Aluno aluno : rato1.getAlunos()){
-          System.out.println(aluno.toString());     
+        for (Aluno aluno : rato1.getAlunos()) {
+            System.out.println(aluno.toString());
         }
         System.out.println("nome: " + rato2.getNome() + "\ntelefone: " + rato2.getTel() + "\nendereço: " + rato2.getEnd());
-        for (Aluno aluno : rato2.getAlunos()){
-          System.out.println(aluno.toString());     
+        for (Aluno aluno : rato2.getAlunos()) {
+            System.out.println(aluno.toString());
         }
  
-
+         
 
     }
- 
+
 }
